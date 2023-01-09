c0fb7c3f282badf16d292b89201d6b6aa663c07c
Inna <Inna.1998@ukr.net>
2023-01-09 22:53:50 +0200

    Закінчила всі зміни

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, {
-        classes: 'One'
-    });
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, {
+//         classes: 'One'
+//     });
+//   });
+
+// $( document ).ready(function) {
+//     $(".sidenav-trigger").dropdown();
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+// });
+
+// $( document ).ready(function() {
+//     $(".sidenav-trigger").dropdown();
+// }) 
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
